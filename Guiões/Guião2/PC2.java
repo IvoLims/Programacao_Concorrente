616e7d07ce22b593ebbad7e6b2e3c69afe8ba57f
IvoLims <75138193+IvoLims@users.noreply.github.com>
2022-03-02 10:46:27 +0000

    Update PC2.java

--- Guiões/Guião2/PC2.java
+++ Guiões/Guião2/PC2.java
@@ -91,15 +91,141 @@ class Incrementer extends Thread{
 
 /* 2. Implemente uma classe Banco que ofereça os métodos da interface abaixo, para crédito, débito e consulta
 do saldo total de um conjunto de contas. Considere um número fixo de contas, definido no construtor do
-Banco, com saldo inicial nulo. Utilize exclusão mútua ao nível do objecto Banco.*/
+Banco, com saldo inicial nulo. Utilize exclusão mútua ao nível do objecto Banco.
 
 interface Bank{
 	void deposit(int id,int val) throws InvalidAccount;
 	void witdraw(int id, int val) throws InvalidAccount, NotEnoughFunds;
 	int totalBalance(int accounts[]) throws InvalidAccount;
-	void transfer(int accounts[]) throws InvalidAccount, NotEnoughFunds;
+	void transfer(int from, int to, int amount) throws InvalidAccount, NotEnoughFunds; //Exercício 3
+} */
+
+public class NotEnoughFunds extends Exception{
+	public NotEnoughFundsException(){super();}
+	public NotEnoughFundsException(String msg){
+		super("Sem Fundos Suficientes "+msg);
+	}
 }
 
-public static void main(String[] args) {
-	
+public class InvalidAccount extends Exception{
+	public InvalidAccountException(){super();}
+	public InvalidAccountException(String msg){
+		super("Conta Invalida "+msg);
+	}
 }
+
+class Bank{
+	private static class Account{
+		private int balance;
+		public int balance(){
+			return balance;
+		}
+		public void deposit(int val){
+			balance += val;
+		}
+		public void witdraw(int val) throws NotEnoughFunds{
+			if (balance < val) throw new NotEnoughFunds();
+			balance -= val;
+		}
+	}
+	public Account[] accounts;
+	private Account get(int id) throws InvalidAccount{
+		if(id < 0 || id >= accounts.length) throw new InvalidAccount();
+		return accounts[id]
+	}
+	public Bank(int n){ 
+		accounts = new Account[n];
+		for(int i=0; i<accounts.length; ++i) accounts[i] = new Account();
+	}
+	public synchronized void deposit(int id,int val) throws InvalidAccount{
+		/* if(id<0 || id >= accounts.length) throw new InvalidAccount();
+		accounts[id].deposit(val);
+
+		Ou get(id).deposit(val);*/
+		Account c = get(id);
+		synchronized(this){
+			c.deposit(val);
+		}
+	}
+	public synchronized void witdraw(int id, int val) throws InvalidAccount, NotEnoughFunds{
+		/* if(id < 0 || id >= accounts.length) throw new InvalidAccount();
+		accounts[id].witdraw(val);
+		Ou get(id).witdraw(val);*/
+		Account c = get(id);
+		synchronized(this){
+			c.witdraw(val);
+		}
+	}
+	public synchronized int totalBalance(int accounts[]) throws InvalidAccount{
+		int total = 0;
+		for(int i = 0; i < accounts.length; i++){
+			if(i < 0 || i >= accounts.length) throw new InvalidAccount();
+			total += accounts[i].balance();
+		}
+		return total;
+		/*
+		Ou
+		int total = 0;
+		for(int id : accounts){
+			total+=get(id).balance();
+			}
+			return total;
+		}
+		*/
+	}
+}
+
+class Depositer extends Thread{
+	final int iterations;
+	final Bank b;
+	Depositor(int iterations, Bank b){ 
+		this.iterations = iterations;
+	    this.b = b;
+	}
+	public void run(){
+		for(int i = 0; i<iterations; ++i){
+			b.deposit(i % b.accounts.length,1);
+		}
+	}
+}
+
+class Main{
+	public static void main(String[] args) throws InterruptedException{
+		final int N = 	Integer.parseInt(args[0]);
+		final int NC = 	Integer.parseInt(args[1]);
+		final int I = Integer.parseInt(args[2]);
+
+		Bank b = new Bank(NC);
+		Thread[] a = new Thread[N];
+		int todasContas = new int[NC];
+
+		for(int i = 0; i<NC; ++i){
+			todasContas[i] = i;
+		}
+
+		/*
+		for(int i = 0; i<NC; ++i){
+			b.deposit(i, 1000);
+		}
+		*/
+
+		for(int i = 0; i<N; ++i){
+			a[i] = new Depositor(I,b);
+		}
+
+		for(int i = 0; i<N; ++i){
+			a[i].start();
+		}
+
+		for(int i = 0; i<N; ++i){
+			a[i].join();
+		}
+		System.out.println(b.totalBalance(todasContas));
+	}
+}
+/* 3. Acrescente o método transferir à classe Banco: 
+
+void transfer(int from, int to, int amount) throws InvalidAccount, NotEnoughFunds;
+
+Considere a viabilidade de este ser implementado simplesmente como a composição sequencial das operações de débito e crédito já implementadas. */
+
